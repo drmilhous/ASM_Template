a096acda364f1298b9b5954f1715b68e8ee62601
EC2 Default User <ec2-user@ip-172-31-24-154.us-east-2.compute.internal>
2024-05-14 16:19:28 +0000

    adding 64 files

---
+++ 64_Bit_Intel/64bit.asm
@@ -0,0 +1,16 @@
+%include "io.inc"
+default rel
+segment .data 
+
+segment .bss 
+
+segment .text
+        global  asm_main
+asm_main:
+        enter   0,0               ; setup routine
+	;***************CODE STARTS HERE***************************
+			
+	;***************CODE ENDS HERE*****************************
+        mov     rax, 0            ; return back to C
+        leave                     
+        ret
