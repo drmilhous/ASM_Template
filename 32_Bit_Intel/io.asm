db39e19d622adaf4db44d66f7a25512af8ec27c1
Matt <matt@milhousink.com>
2024-05-29 14:12:56 +0000

    updating io libs

--- 32_Bit_Intel/io.asm
+++ 32_Bit_Intel/io.asm
@@ -1,13 +1,113 @@
-;RDI, RSI, RDX, RCX, R8, R9 (R10) XMM0, XMM1, XMM2, XMM3, XMM4, XMM5, XMM6 and XMM7 are used for the first floating point arguments.
+; ;RDI, RSI, RDX, RCX, R8, R9 (R10) XMM0, XMM1, XMM2, XMM3, XMM4, XMM5, XMM6 and XMM7 are used for the first floating point arguments.
 segment .data
-printInt: db "%d",0
-printNewline: db 10,0
-printString: db "%s",0
-dump_regs_string: db "RAX =0x%16X", 10,0
+print_int_format db "%d",10,0 
+print_hex_format db "%08X",10,0
+scan_int_format db "%d", 0
+print_string_format db "%s", 0
+
+
+; printInt: db "%d",0
+; printNewline: db 10,0
+; printString: db "%s",0
+; dump_regs_string: db "RAX =0x%16X", 10,0
+; global print_nl
+; global print_string
+; global dump_regs
+
 extern printf
+extern scanf
+extern puts
+
 
 segment .text 
-global print_int 
-global print_nl
-global print_string
-global dump_regs
+    global print_int 
+    global print_hex
+    global read_int
+    global print_string
+
+    global get_GOT_EAX
+    extern  _GLOBAL_OFFSET_TABLE_
+
+
+
+
+%macro  get_GOT 0 
+        call    %%getgot 
+  %%getgot: 
+        pop     ebx 
+        add     ebx,_GLOBAL_OFFSET_TABLE_+$$-%%getgot wrt ..gotpc 
+%endmacro
+%macro  get_GOT_A 0 
+        call    %%getgot_a 
+  %%getgot_a: 
+        pop     eax 
+        add     eax,_GLOBAL_OFFSET_TABLE_+$$-%%getgot_a wrt ..gotpc 
+%endmacro
+
+get_GOT_EAX:
+        get_GOT_A
+        ret
+
+print_int:
+        push   ebp
+        mov    ebp, esp
+        push   eax
+        push   ebx
+
+        push eax
+        ; -no-pie 
+        get_GOT
+        lea     eax,[ebx+print_int_format wrt ..gotoff]
+        push eax
+        call printf WRT ..plt
+        add esp, 8
+
+        pop ebx
+        pop eax
+        pop ebp
+        ret
+print_hex:
+        push   ebp
+        mov    ebp, esp
+        push   eax
+        push ebx
+
+        push eax
+        ; -no-pie 
+        get_GOT
+        lea     eax,[ebx+print_hex_format wrt ..gotoff]
+        push eax
+        call printf WRT ..plt
+        add esp, 8
+        pop ebx
+        pop eax
+        pop ebp
+        ret
+read_int:
+        push    ebp
+        mov     ebp, esp
+        sub     esp, 4
+        lea     eax, [ebp-4]
+        push    eax
+        call    get_GOT_EAX
+        lea     eax,[eax+scan_int_format wrt ..gotoff]
+        push    eax
+        call    scanf WRT ..plt
+        add     esp, 8
+
+        mov eax, [ebp-4]
+        add     esp, 4
+        mov     esp, ebp
+        pop     ebp
+        ret
+print_string:
+        push ebp
+        mov ebp, esp
+        push eax
+        call    get_GOT_EAX
+        lea     eax,[eax+print_string_format wrt ..gotoff]
+        push eax
+        call printf WRT ..plt
+        add esp, 8
+        pop ebp
+        ret
--- 64_Bit_Intel/io.asm
+++ 64_Bit_Intel/io.asm
@@ -3,35 +3,83 @@ segment .data
 printInt: db "%d",0
 printNewline: db 10,0
 printString: db "%s",0
-dump_regs_string: db "RAX =0x%16X", 10,0
+
+dumpMessage db "RAX: %016lX RBX: %016lX RCX: %016lX RDX: %016lX R8 : %016lX R9 : %016lX", 10,
+dl2 db "R10: %016lX R11: %016lX R12: %016lX R13: %016lX R14: %016lX R15: %016lX", 10
+dl3 db "RSI: %016lX RDI: %016lX RSP: %016lX RBP: %016lX", 10,0
+
 extern printf
 
 segment .text 
 global print_int 
 global print_nl
 global print_string
-global dump_regs
+global dump_registers
+dump_registers:
+        ;stack before call + 0x18
+        ; return address + 0x10
+        push rbp; rbp + 8
+        mov rbp, rsp
+        push rsi
+        push rdi
+        push rax
+        push rbx
+        push rcx
+        push rdx
+        push r8
+        push r9
+        push r10
+        push r11
+        push r12
+        push r13
+        push r14
+        push r15
+        sub rsp, 0x8
+        sub rsp, 0x10
+        ; + 28h
+        ; + 10h
+        push rdi; + 0x8
+        push rsi; +0
+        mov rsi, rbp
+        add rsi, 0x18
+        mov [rsp + 0x10], rsi ; rsp
+        mov rsi, rbp
+        mov rdi, [rsi]
+        mov [rsp + 0x18], rdi; rbp
+        push r15;     0x38
+        push r14;     0x30
+        push r13;     0x28
+        push r12 ;    0x20
+        push r11;     0x18       
+        push r10;16 = 0x10
+        push r9;8
+        mov r9, r8
+        mov r8,  rdx
+        mov rcx, rcx; same
+        mov rdx, rbx
+        mov rsi, rax
+        mov rdi, dumpMessage
+        call printf WRT ..plt
+        add rsp, 0x58
 
-dump_regs:
-	enter 0,0
-	push rdi
-	push rsi
-	push rdx
-	push rcx	
-	push r9
+        add rsp, 0x8
+        pop r15
+        pop r14
+        pop r13
+        pop r12
+        pop r11
+        pop r10
+        pop r9
+        pop r8
+        pop rdx
+        pop rcx
+        pop rbx
+        pop rax
+        pop rdi
+        pop rsi
+        pop rbp
+        ret
 
-	mov rsi, rax
-	mov rdi, dump_regs_string
-	call printf WRT ..plt
-
-
-	pop r9
-	pop rcx
-	pop rdx
-	pop rsi
-	pop rdi
-	leave
-	ret
 
 print_int:
 	push rax
