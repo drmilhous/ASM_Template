6a0085b65a3c5f0cac2af85888086fc1606638d8
Matt <matt@milhousink.com>
2024-06-04 20:29:05 +0000

    adding printbits and include main.c in build

--- 64_Bit_Intel/io.asm
+++ 64_Bit_Intel/io.asm
@@ -4,18 +4,24 @@ default rel
 printInt: db "%d",0
 printNewline: db 10,0
 printString: db "%s",0
+hex_format: db "%*X", 0 
+space: db " ", 0
+
 
 dumpMessage db "RAX: %016lX RBX: %016lX RCX: %016lX RDX: %016lX R8 : %016lX R9 : %016lX", 10,
 dl2 db "R10: %016lX R11: %016lX R12: %016lX R13: %016lX R14: %016lX R15: %016lX", 10
 dl3 db "RSI: %016lX RDI: %016lX RSP: %016lX RBP: %016lX", 10,0
 
 extern printf
+extern putchar
 
 segment .text 
 global print_int 
 global print_nl
 global print_string
 global dump_registers
+global print64Hex
+global printBits
 dump_registers:
         ;stack before call + 0x18
         ; return address + 0x10
@@ -115,3 +121,87 @@ print_string:
 	pop rsi
 	pop rax
 	ret
+
+printBits:
+        push rbp ; prolouge
+        mov rbp, rsp
+        push rsi; save my used registers
+        push rdi
+        push r12
+        push r13
+        push r14
+        push rdx
+        mov r12, 0x8000000000000000; initial bit mask
+        ;0x1000000000000000
+        ;0x0100000000000000
+        ;  r12 000000010000000000000
+        ; rsi  101010100101010100101
+        ; rsi  000000000000000000000
+        ;  r12 000000100000000000000
+        ; rsi  101010100101010100101
+        ; rsi  000000100000000000000
+
+        mov r13, rdi ; mov arg 1 to r13 
+        mov r14, 4; counter
+
+        top:    cmp r12, 0
+                jz done
+                        mov rsi, r13 ; copy to here, so don't destroy
+                        and rsi, r12; setting rdx to 0 or 1 
+                        mov rax, '0'
+                        cmovz rdi, rax
+                        mov rax, '1'
+                        cmovnz rdi, rax
+                        call putchar  wrt ..plt
+                        shr r12, 1
+                        sub r14, 1 ; if counter == 0
+                        cmp r14,0
+                        jnz top
+                        mov rdi, ' '
+                        call putchar wrt ..plt
+                        mov r14, 4 ; reset counter
+                        jmp top
+        done:
+        call print_nl
+        mov rsi, 4
+        mov rdi,r13
+        call print64Hex
+        pop rdx
+        pop r14
+        pop r13
+        pop r12
+        pop rdi
+        pop rsi
+        pop rbp
+        ret
+print64Hex:
+        push rbp
+        mov rbp, rsp
+        mov r15, rdi; R15 has the value
+        mov r13,0xf000000000000000
+        mov rcx, 64; initial counter
+        hex_test:
+                sub rcx, 4; shift counter
+                mov r14, r13 ; get the mask 
+                and r14, r15
+                shr r14, cl
+                mov rdx, r14
+                ; mov rsi, 1
+                ; mov rsi, r14
+                push rcx
+                push rsi; argument gives the number of spaces
+                lea rdi, hex_format
+                call printf wrt ..plt
+                lea rdi, space
+                call printf wrt ..plt
+                pop rsi
+                pop rcx
+                shr r13, 4
+                cmp r13, 0
+                jz hex_done        
+                jmp hex_test
+        hex_done:
+        call print_nl
+        mov rsp, rbp
+        pop rbp
+        ret
