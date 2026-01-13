b7bb54eb9f409a30c1119e9940d2b8bb89bf4262
Matt Miller <matt@milhousink.com>
2026-01-13 16:23:02 -0600

    adding example docker files

---
+++ bin/dockerbuild.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+# docker build -f DockerFile -t asm-template .
+docker-compose build
+# docker run -it --rm asm-template
---
+++ bin/dockerrun.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+# Run the container using docker-compose
+docker-compose run --rm asm
+
+# docker-compose -f ./docker-compose.yml -p x up -d
