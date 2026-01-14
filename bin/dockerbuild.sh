1656a07e3c33f9cd6dfa0b8d3f0cf33a1726a356
Matt Miller <millermj@famgenix.com>
2026-01-14 16:51:21 -0600

    adding 32bit arm and mac for docker

--- bin/dockerbuild.sh
+++ bin/dockerbuild.sh
@@ -1,4 +1,9 @@
 #!/bin/bash
 # docker build -f DockerFile -t asm-template .
-docker-compose build
+#check if mac
+if [[ "$OSTYPE" == "darwin"* ]]; then
+    docker compose build
+else
+    docker-compose build
+fi
 # docker run -it --rm asm-template
--- bin/dockerrun.sh
+++ bin/dockerrun.sh
@@ -1,5 +1,10 @@
 #!/bin/bash
 # Run the container using docker-compose
-docker-compose run --rm asm
+DOCKER_COMMAND=$(which docker-compose)
+if [[ "$OSTYPE" == "darwin"* ]]; then
+    docker compose run --rm asm
+else
+    docker-compose run --rm asm
+fi
 
 # docker-compose -f ./docker-compose.yml -p x up -d
